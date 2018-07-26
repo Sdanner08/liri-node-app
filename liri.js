70543eb0fe8d7ffdbbeadc8cc9716135726edbed
Shane Danner <sdanner08@yahoo.com>
2018-07-26 19:04:53 -0400

    added more

--- liri.js
+++ liri.js
@@ -1,13 +1,30 @@
 require("dotenv").config();
 var Twitter = require('twitter');
+var Spotify = require('node-spotify-api');
 
 var keys = require("./keys.js");
-console.log(keys)
 
-//var spotify = new Spotify(keys.spotify);
+//User input
+var userInput = process.argv[2];
+var userChoice = process.argv[3];
+
+var spotify = new Spotify(keys.spotify);
 var client = new Twitter(keys.twitter);
 
 
+//Movie Info--- still need to add it.
+var movieTitle;
+var movieYear;
+var movieRating;
+var movieCountry;
+var movieLang;
+var moviePlot;
+var movieActor;
+var tomatoRating;
+var tomatoURL;
+
+
+//need to work on refining the information given by the twitter API
 var params = {screen_name: '@theymademedoit6'};
 client.get('statuses/user_timeline', params, function(error, tweets, response) {
   if (!error) {
@@ -15,3 +32,16 @@ client.get('statuses/user_timeline', params, function(error, tweets, response) {
   }
 });
 
+
+ //need to work on refining the search
+spotify
+  .search({ type: 'track', query: 'All the Small Things' })
+  .then(function(response) {
+    console.log(response);
+  })
+  .catch(function(err) {
+    console.log(err);
+  });
+
+
+  //movie search items
